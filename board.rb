1c707271e6fb4d12049c6f0131c020353d84aa26
maybethee <jozalamun@gmail.com>
2023-08-30 20:19:01 -0400

    Write code for check_win_rows tests

--- board.rb
+++ board.rb
@@ -39,19 +39,32 @@ class Board
     end
   end
 
-  # def check_win_rows
-
-  # end
+  def check_win_rows
+    @grid.each do |row|
+      # in each row iterate over 4-long subarrays
+      row.each_cons(4) do |consecutive|
+        return true if consecutive.uniq.size == 1 && consecutive.first != 'O'
+      end
+    end
+    # no win found
+    false
+  end
 
   # def check_win_columns
 
   # end
 
   def check_win_diagonals
+    # iterate over rows
     6.times do |row|
+      # iterate over columns
       7.times do |col|
+        # diagonal1 checks in one direction
         diagonal1 = (0..3).collect { |i| @grid[row + i][col + i] if row + i < 6 && col + i < 7 }
+        # diagonal2 checks the other direction
         diagonal2 = (0..3).collect { |i| @grid[row + i][col - i] if row + i < 6 && col - i >= 0 }
+
+        # uses uniq to check matching based on the first character in diagonal
         return true if diagonal1.uniq.size == 1 && %w[R Y].include?(diagonal1.first)
         return true if diagonal2.uniq.size == 1 && %w[R Y].include?(diagonal2.first)
       end
